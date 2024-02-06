f62779003454681971fc7a1553ddc8f964a915bc
Coulter <coultercash@gmail.com>
2024-02-06 08:43:26 -0700

    [PARSER]: Create New Parse Line Function

--- src/parser.cpp
+++ src/parser.cpp
@@ -12,6 +12,10 @@ struct parserSettings
 class Parser{
     public:
         parserSettings ParserSettings;
+
+        int parse_line(std::string& line, int line_number, std::ifstream& file_path){
+
+        }
     
         int parse(){
             if (ParserSettings.isFile) {
@@ -19,6 +23,13 @@ class Parser{
                 if (!file.is_open()){
                     std::cerr << "CSL: Parser Failed Trying to Open Script: " << ParserSettings.filepath << std::endl;
                     return;
+                }
+
+                std::string line;
+                int i = 0;
+                while (std::getline(file, line)) {
+                    i +=1;
+                    parse_line(line, i, file);
                 }
             } 
         }
