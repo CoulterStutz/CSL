a919b3d9741209c62e421e8bf31987d52c9185ad
Coulter <coultercash@gmail.com>
2024-02-06 08:40:07 -0700

    [MAIN]: Handle Arguments

---
+++ src/csl.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]){
+    if (argc == 1) {
+        
+        while (true) {
+            std::string cmd;
+            printf("CSL>>: ");
+            std::cin >> cmd;
+        }
+    }
+    return 0;
+}
